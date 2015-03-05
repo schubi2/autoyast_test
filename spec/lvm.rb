ed949b3c34fbe1db921a0a69a705d1a09ac95d4e
Stefan Schubert <schubi@suse.de>
2015-03-05 08:40:18 +0100

    running as root

--- spec/lvm.rb
+++ spec/lvm.rb
@@ -12,7 +12,7 @@ describe "SLES 12 with LVM partition" do
     if File.exists?(shell)
       # Copy the file to be tested to /tmp inside the booted box and execute it.
       $vm.inject_file(shell, "/tmp")
-      actual = $vm.run_command("source /tmp/#{File.basename(shell)}", stdout: :capture, as: "vagrant")
+      actual = $vm.run_command("source /tmp/#{File.basename(shell)}", stdout: :capture, as: "root")
 
       # Compare the expected value.
       expected = "AUTOYAST OK"
