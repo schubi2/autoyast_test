bea46625ed515b421309aa6582b72801e5a644de
Stefan Schubert <schubi@suse.de>
2015-03-06 10:30:52 +0100

    unify

--- spec/sles12.rb
+++ spec/sles12.rb
@@ -5,11 +5,28 @@ describe "SLES 12 installation" do
   before(:all) do
     # Start the previously create vagrant VM - opensuse_vm. 
     $vm = start_system(box: "autoyast_vm")
+    $base_dir = File.dirname(__FILE__)
   end
 
   it "checks, if user -vagrant- has been created" do
-    shell = __FILE__.gsub(".rb",".sh")
+    shell =  File.join($base_dir, "user.sh")
     if File.exists?(shell)
+      # Copy the file to be tested to /tmp inside the booted box and execute it.
+      $vm.inject_file(shell, "/tmp")
+      actual = $vm.run_command("source /tmp/#{File.basename(shell)}", stdout: :capture, as: "vagrant")
+
+      # Compare the expected value.
+      expected = "AUTOYAST OK"
+      expect(actual.split("\n").last).to eq(expected)
+    end
+  end
+
+  it "checks, if files can be downloaded from tftp server" do
+    shell =  File.join($base_dir, "tftp.sh")
+    if File.exists?(shell)
+      # Copy download test file into the system
+      $vm.inject_file(shell, "/srv/tftpboot/test.txt")
+
       # Copy the file to be tested to /tmp inside the booted box and execute it.
       $vm.inject_file(shell, "/tmp")
       actual = $vm.run_command("source /tmp/#{File.basename(shell)}", stdout: :capture, as: "vagrant")
--- spec/tftp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e -x
-
-time tftp localhost -c get test.txt test2.txt || \
-time tftp localhost -c get test.txt test2.txt
-
-diff -u /srv/tftpboot/test.txt test2.txt && echo "AUTOYAST OK"
-
