2956ab1c88730506c629b255bd10aa182dd2510a
Stefan Schubert <schubi@suse.de>
2015-03-13 14:38:02 +0100

    added dns test

--- spec/sles12.rb
+++ spec/sles12.rb
@@ -37,6 +37,20 @@ describe "SLES 12 installation" do
     end
   end
 
+  it "checks, dns server" do
+    shell =  File.join($base_dir, "dns.sh")
+    if File.exists?(shell)
+      # Copy the file to be tested to /tmp inside the booted box and execute it.
+      $vm.inject_file(shell, "/tmp")
+      actual = $vm.run_command("source /tmp/#{File.basename(shell)}", stdout: :capture, as: "root")
+
+      # Compare the expected value.
+      expected = "AUTOYAST OK"
+      expect(actual.split("\n").last).to eq(expected)
+    end
+  end
+
+
   after(:all) do
     # Shutdown the vagrant box.
     $vm.stop
