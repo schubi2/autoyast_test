e371e1a7fd09ae27e4f600f95102c716821acd2a
Stefan Schubert <schubi@suse.de>
2015-03-17 10:16:34 +0100

    added firewall

--- spec/http.rb
+++ spec/http.rb
@@ -1,13 +1,13 @@
 require_relative "../helper/spec_helper.rb"
 
-describe "SLES 12 HTTP server (apache2)" do
+describe "SLES 12 HTTP server " do
 
   before(:all) do
     # Start the previously create vagrant VM - opensuse_vm. 
     $vm = start_system(box: "autoyast_vm")
   end
 
-  it "checks, if http runs" do
+  it "checks, apache2 and firewall is running correctly" do
     run_test_script("http.sh")
   end
 
