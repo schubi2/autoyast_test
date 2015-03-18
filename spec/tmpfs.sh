d4cade74d5cf996b5e1730df45393ba87aa8367c
Stefan Schubert <schubi@suse.de>
2015-03-18 13:27:49 +0100

    correct access to variable

--- spec/tmpfs.sh
+++ spec/tmpfs.sh
@@ -4,7 +4,7 @@ set -e -x
 
 NO_TMP_DEVICE=1
 
-# Checking that there is no tmpfs device defined
-grep "<device>/dev/tmpfs</device>" /root/autoinst.xml && $NO_TMP_DEVICE=0
+# Checking if there is no tmpfs device defined
+grep "<device>/dev/tmpfs</device>" /root/autoinst.xml && NO_TMP_DEVICE=0
 
 test $NO_TMP_DEVICE -eq 1 && echo "AUTOYAST OK"
